14753bd714d5bf139e211a7ca812fd78e8c71615
Mallory Steffes <mallorysteffes@hotmail.com>
2018-08-04 14:29:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-document.addEventListener("DOMContentloaded", (e) => {
-  // invoking a function here will make sure it happens on page load
-  // your code here
-}
+$(document).ready(function() {
+  debugger;
+})
